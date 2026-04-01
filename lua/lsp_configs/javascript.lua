18c16456c712aa60fc7e1be3279b13d9ca1f0ded
Gabriel Costa Martins Ganassin <gabricostam@gmail.com>
2026-04-01 09:32:41 -0300

    fix(lsp): use root_markers for denols config

--- lua/lsp_configs/javascript.lua
+++ lua/lsp_configs/javascript.lua
@@ -14,7 +14,7 @@ return function()
 	})
 
 	vim.lsp.config("denols", {
-		root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc"),
+		root_markers = { "deno.json", "deno.jsonc" },
 	})
 
 	vim.lsp.config("vtsls", {
